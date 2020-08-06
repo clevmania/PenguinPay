0e86902be88f6884faa59c2e3ef204476a49acab
clevmania <ernestcleverest@yahoo.com>
2020-08-06 16:16:24 +0100

    setup navigation

--- app/src/main/java/com/clevmania/penguinpay/ui/sendmoney/SendMoneyFragment.kt
+++ app/src/main/java/com/clevmania/penguinpay/ui/sendmoney/SendMoneyFragment.kt
@@ -26,7 +26,7 @@ class SendMoneyFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SendMoneyViewModel::class.java)
+//        viewModel = ViewModelProviders.of(this).get(SendMoneyViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
